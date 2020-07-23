ecb0ec511d6350cb5582f211733ed0bd3689c7b4
Ami Cai <ami.cai333@gmail.com>
2020-07-23 16:55:30 -0400

    Starting and Adding Spawner Code

---
+++ Angel and Devil Game/Assets/Scripts/Hazards.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazards : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+                //this puts a message in the Console that the impact has hit
+            Debug.Log("Collided with Ground");
+                //This is script that makes the object disappear when hitting the ground!
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Angel and Devil Game/Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{ 
+    [SerializeField]
+    //used for single enemy spawn
+    private float xLimit;
+
+    [SerializeField]
+    //redeine positions
+    private float[] xPositions;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
