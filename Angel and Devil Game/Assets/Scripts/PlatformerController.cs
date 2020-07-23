5a6297fa7f399c72703c02b97f39171fc24d45c2
Ami Cai <ami.cai333@gmail.com>
2020-07-23 15:34:32 -0400

    Movable Player and Falling
    • Added Platformer Script + edits
    • Created Falling script for assets

---
+++ Angel and Devil Game/Assets/Scripts/PlatformerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerController : MonoBehaviour
+{
+    [SerializeField]
+    //This number is from dragging player to edge of screen, the X value of the screen + player
+    private float xLimit = 2.64f;
+
+    //Speed of character movement and height of the jump. Set these values in the inspector.
+    public float speed;
+
+    //Assigning a variable where we'll store the Rigidbody2D component.
+    private Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Sets our variable 'rb' to the Rigidbody2D component on the game object where this script is attached.
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this will love the character's movement in both positive and negative x values, y value, and z.
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -xLimit, xLimit), transform.position.y, transform.position.z);
+
+        //Movement code for left and right arrow keys.
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(+speed, rb.velocity.y);
+        }
+        
+    }
+
+
+}
