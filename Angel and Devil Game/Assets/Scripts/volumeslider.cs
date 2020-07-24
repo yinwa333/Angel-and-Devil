630d6e870300b8cc48a282c3919e8e031b56d473
Midi <midimayo.art@gmail.com>
2020-07-24 11:48:02 -0400

    Volume Slider Control
    added a volume slider that controls the volume, and a mixer for the general BGM

---
+++ Angel and Devil Game/Assets/Scripts/volumeslider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class volumeslider : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+
+   public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", Mathf.Log10 (volume) * 20);
+        Debug.Log(volume);
+    }
+}
