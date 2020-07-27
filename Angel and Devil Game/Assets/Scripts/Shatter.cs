00bbf3d1f877310abed40cefe7665b67bbdca174
Ami Cai <ami.cai333@gmail.com>
2020-07-27 13:42:14 -0400

    Random Falling Waves!
    Functioning Scripts: Shatter, Falling, and Spawner

--- Angel and Devil Game/Assets/Scripts/Shatter.cs
+++ Angel and Devil Game/Assets/Scripts/Shatter.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hazards : MonoBehaviour
+public class Shatter : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Ground"))
         {
-                //this puts a message in the Console that the impact has hit
-            Debug.Log("Collided with Ground");
+                //this puts a message in the Console that the impact has hit, i took it off because idc
+            //Debug.Log("Collided with Ground");
                 //This is script that makes the object disappear when hitting the ground!
             gameObject.SetActive(false);
         }
--- Angel and Devil Game/Assets/Scripts/Spawner.cs
+++ Angel and Devil Game/Assets/Scripts/Spawner.cs
@@ -9,16 +9,79 @@ public class Spawner : MonoBehaviour
     private float xLimit;
 
     [SerializeField]
-    //redeine positions
+    //redefine positions
     private float[] xPositions;
+    //
+    [SerializeField]
+    private GameObject[] prefabs;
+    //
+    [SerializeField]
+    private Wave[] wave;
+
+    private float currentTime;
+
+    List<float> remainingPositions = new List<float>();
+    private int waveIndex;
+    float xPos = 0;
+    int rand;
 
     void Start()
     {
-        
+        currentTime = 0;
+        remainingPositions.AddRange(xPositions);
     }
 
     void Update()
     {
-        
+        currentTime -= Time.deltaTime;
+        if (currentTime <= 0)
+        {
+            SelectWave();
+        }
     }
+
+    void SpawnEnemy(float xPos)
+    {
+        // 0,# types of objects
+        int r = Random.Range(0, 1);
+        GameObject enemyObj = Instantiate(prefabs[r], new Vector3(xPos, transform.position.y, 0), Quaternion.identity);
+
+    }
+
+    //Spawn Wave
+    void SelectWave()
+    {
+        remainingPositions = new List<float>();
+        remainingPositions.AddRange(xPositions);
+
+        waveIndex = Random.Range(0, wave.Length);
+
+        currentTime = wave[waveIndex].delayTime;
+
+        if (wave[waveIndex].spawnAmount == 1)
+            xPos = Random.Range(-xLimit, xLimit);
+        else if (wave[waveIndex].spawnAmount > 1)
+        {
+            rand = Random.Range(0, remainingPositions.Count);
+            xPos = remainingPositions[rand];
+            //so it doesn't do the same wave beforehand
+            remainingPositions.RemoveAt(rand);
+        }
+
+        for (int i = 0; i < wave[waveIndex].spawnAmount; i++)
+        {
+            SpawnEnemy(xPos);
+            rand = Random.Range(0, remainingPositions.Count);
+            xPos = remainingPositions[rand];
+            remainingPositions.RemoveAt(rand);
+        }
+    }
+}
+
+
+[System.Serializable]
+public class Wave
+{
+    public float delayTime;
+    public float spawnAmount;
 }
